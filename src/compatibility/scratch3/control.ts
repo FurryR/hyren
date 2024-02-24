6f853d5b8c554f58fc2e5cda712769e5c391306b
FurryR <awathefox@gmail.com>
2024-02-24 23:35:32 +0800

    :art: Format

--- src/compatibility/scratch3/control.ts
+++ src/compatibility/scratch3/control.ts
@@ -1,10 +1,11 @@
 export default function scratch3Control(packageObject: any) {
   if (!packageObject._counter) packageObject._counter = 0
-  if (!packageObject._createClone) packageObject._createClone = function (
-    this: any,
-    cloneOption: string,
-    target: VM.RenderedTarget
-  ) {
-    this.createClone({ CLONE_OPTION: cloneOption }, { target })
-  }
+  if (!packageObject._createClone)
+    packageObject._createClone = function (
+      this: any,
+      cloneOption: string,
+      target: VM.RenderedTarget
+    ) {
+      this.createClone({ CLONE_OPTION: cloneOption }, { target })
+    }
 }
